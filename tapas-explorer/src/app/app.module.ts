eaa3773a3c5da822dcbbe9cf37f187d9291b476c
Markus Weser <git@markusweser.info>
2018-03-16 11:59:35 +0000

    feat: @angular/material

--- tapas-explorer/src/app/app.module.ts
+++ tapas-explorer/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+
+import { MaterialModule } from './material.module';
 
 
 @NgModule({
