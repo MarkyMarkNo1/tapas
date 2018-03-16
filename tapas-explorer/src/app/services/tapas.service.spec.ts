6787f81c7635d2b329f288e7b1f18eeaa487253a
Len Paalvast <l.paalvast@portbase.com>
2018-03-16 11:35:35 +0000

    added empty tapas service

---
+++ tapas-explorer/src/app/services/tapas.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { TapasService } from './tapas-.service';
+
+describe('TapasService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TapasService]
+    });
+  });
+
+  it('should be created', inject([TapasService], (service: TapasService) => {
+    expect(service).toBeTruthy();
+  }));
+});
