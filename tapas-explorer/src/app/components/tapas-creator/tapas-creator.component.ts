a8a6c3d9407a45ef74ac5c6f37f61e2f4d572400
Markus Weser <git@markusweser.info>
2018-03-17 11:14:12 +0000

    feat: save tapa

--- tapas-explorer/src/app/components/tapas-creator/tapas-creator.component.ts
+++ tapas-explorer/src/app/components/tapas-creator/tapas-creator.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 
+import { Router } from '@angular/router';
+import { TapasService } from '../../services/tapas.service';
+import { Tapa } from '../../../model/tapas';
+
+
 @Component({
   selector: 'tap-tapas-creator-component',
   templateUrl: './tapas-creator.component.html',
   styleUrls: ['./tapas-creator.component.css']
 })
-export class TapasCreatorComponent implements OnInit {
+export class TapasCreatorComponent {
 
-  form: FormGroup;
+  categories = ['Meat', 'Vegetarian', 'Vegan'];
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private router: Router, private tapasService: TapasService) {}
 
-  ngOnInit() {
-    this.form = this.formBuilder.group({});
-
+  save(value: Tapa) {
+    this.tapasService.saveTapas(value)
+      .subscribe(() => this.router.navigate(['/tapa/list']));
   }
 
 }
